aa8f21eb308cd9157def84a23b759ff011c7fabd
ANTONIO <jranjas00@gmail.com>
2023-08-30 00:17:49 +0700

    latihan NGC

---
+++ h8dsft_ngc_basic_data_wrangling_in_sql.ipynb.sql
@@ -0,0 +1,16 @@
+-- SELECT * FROM customer;
+
+-- SELECT * FROM "order";
+
+-- insert into customer (customer_name, city)
+-- values('anjas','jakarta');
+
+-- select * from customer
+-- inner join "order" on costomer_id = costomer_id
+
+-- select
+-- 	costomer_id
+-- from 
+-- 	customer
+-- group by
+-- 	costomer_id ;
